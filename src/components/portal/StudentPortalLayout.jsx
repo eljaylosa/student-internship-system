cf1b76a2840484c1c482449d244c3e1db242edba
eljaylosa <eglosa24@bpsu.edu.ph>
2026-08-13 17:10:53 +0800

    Add warning message about dummy data in StudentPortalLayout component

--- src/components/portal/StudentPortalLayout.jsx
+++ src/components/portal/StudentPortalLayout.jsx
@@ -310,6 +310,12 @@ const StudentPortalLayout = () => {
           {/* NAVIGATION */}
 
           <div className="flex-1 overflow-y-auto px-3 py-4">
+            <h3 className="mb-4 p-2 bg-red-500">
+              Note: All data on this page are all dummy data's*
+              <br /> 
+              No real data's are used in this project.
+              No Database is implemented yet
+            </h3>
             <nav className="space-y-1">
               {sidebarItems.map((item) => {
                 const hasChildren = item.children?.length > 0;
